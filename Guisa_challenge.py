0d73da979aa039a9934713458364069d9d837b59
Eric Guisa <ericgui.71375@redlandsschools.net>
2018-09-18 05:57:20 +0000

    final

--- Guisa_challenge.py
+++ Guisa_challenge.py
@@ -1,8 +1,16 @@
+name =input('What is your name? ')
+hi = input('Hello '+ name +' Do you have a traingle for me? ')
+if hi ==('yes'):
+    print('yay')
 a = int(input('Please enter side a: '))
 b = int(input('Please enter side b: '))
 c = int(input('Please enter side c: '))
 
 if (a**2 + b**2 == c**2):
     print("this is a right triangle")
+if (a**2 + b**2 < c**2):
+    print("this is an obtuse triangle")
+if (a**2 + b**2 > c**2):
+    print("this is an acute triangle")
 else:
-    print("dis no right triangle")
+    print("dis no triangle")
